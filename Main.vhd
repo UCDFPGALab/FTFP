497d2f8056f56f5f327a73c09b3b39c26cec6265
Boris P <borpo@ucdavis.edu>
2013-09-06 13:39:12 -0700

    Commit before I delete the whole old algorithm to add stuff, so I can implement simple_algs in a pipeline fashion

--- Main.vhd
+++ Main.vhd
@@ -6,6 +6,9 @@ use IEEE.NUMERIC_STD.ALL;
 library UNISIM;
 use UNISIM.VComponents.all;
 
+library work;
+use work.constants.all;
+
 entity Main is
 	port(
 		SYSCLK_P : in std_logic;
@@ -121,35 +124,6 @@ architecture Behavioral of Main is
 				dataOut  : out unsigned(INTSIZE*ROWS*COLUMNS-1 downto 0);
 				valid    : out std_logic);
 	end component;
-	
-	--********************************************************************************************--
-	--****************************************CONSTANT********************************************--
-	--********************************************************************************************--
-	
-	
-	--RS-232 Constants
-	constant DIVIDER : integer := 1736;
-	constant STOPBITS : integer := 1;
-	constant DATABITS : integer := 8;
-	constant PARITY : boolean := false; --not implemented yet, keep false
-	
-	--Buffer constants
-	constant SIZEOFCELL : integer := 128; -- size of each cell in the buffer (in bits)
-	constant SIZEOFBUFFER : integer := 3; -- number of cells in buffer
-	
-	--PreBuffer constants
-	--Stable configs: SIZEOFCELL/SIZEOFBUFFER/SETUP/HOLD (8/300/15/5)
-	constant SETUP : integer := 15; --how many clock cycles to keep the data valid before sending the "store" signal
-	constant HOLD : integer := 5; --how many clock cycles to keep the data valid after sending the "store" signal
-	
-	--Disassembler/assembler constants
-	constant BROKENBITS   : integer := 8;
-	constant	LUMPBITS  : integer := SIZEOFCELL;
-	constant DELAY1  : integer := 15;
-	constant DELAY2 : integer := 2;
-	
-	--Algorithm constants
-	constant INTSIZE : integer := 8; --size of each int in bytes
 	
 	--********************************************************************************************--
 	--****************************************SIGNALS*********************************************--
